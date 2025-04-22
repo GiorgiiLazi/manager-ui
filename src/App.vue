db36e77cf353a9d611e9fd6cf12565b8ed3bd40d
GiorgiiLazi <malazi140@gmail.com>
2025-04-22 17:53:27 +0400

    light mode is done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Mode @changeMode = 'handleLightMode' />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mode from './components/Mode.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Mode
+  },
+  methods:{
+    handleLightMode(data){
+      console.log(data)
+      if(data === true){
+        document.documentElement.classList.remove('dark')
+        document.documentElement.classList.add('light')
+      }
+      else{
+        document.documentElement.classList.add('dark')
+        document.documentElement.classList.remove('light')
+      }
+    }
+  },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+html {
+  background: rgb(9, 15, 45);
+  font-family: "Oswald";
+  width: 100%;
+  box-sizing: border-box;
 }
+.light{
+  background: rgb(109, 129, 211);
+}
+.dark{
+  background: rgb(9, 15, 45);
+}
+
 </style>
---
+++ src/components/Mode.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <div class="title-img">
+        <img src="@/assets/logo.svg" alt="Logo" />
+      </div>
+    </div>
+
+    <div class="light-mode">
+      <span 
+      v-if="lightMode" 
+      @click="changeMode"
+      class="material-symbols-outlined">
+        light_mode
+      </span>
+      <span 
+      v-else
+      @click="changeMode"
+      class="material-symbols-outlined"
+      >
+        dark_mode
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['changeMode'])
+const lightMode = ref(true)
+
+const changeMode = () =>{
+  lightMode.value =! lightMode.value
+  
+  emit('changeMode', lightMode.value)
+}
+
+</script>
+
+<style>
+.container{
+  background: rgba(204, 204, 204, 0.5);
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 50px;
+  border-radius: 8px;
+  font-size: 1.2em
+}
+.title{
+  max-width: 500px;
+}
+.title >*{
+  color: white;
+  margin: 0 10px;
+  padding: 20px;
+}
+.light-mode span{
+  cursor: pointer;
+  margin: 0 10px;
+  padding: 20px;
+}
+</style>
